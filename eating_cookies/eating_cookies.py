e73569f340169f345c2370ce456c76b1156cab21
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-01-13 20:13:30 +0800

    Complete eating_cookies exercise
    Optimize solution using memoization
    
    Modify test parameters to evaluate memoization solution

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,21 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+def eating_cookies(n, cache={}):
+  if n in cache.keys():
+    return cache[n]
+
+  if n <= 1:
+    return 1
+  elif n == 2:
+    return 2
+  elif n == 3:
+    return 4
+
+  result = eating_cookies(n - 3, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 1, cache)
+  cache[n] = result
+
+  return result
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
@@ -14,3 +27,5 @@ if __name__ == "__main__":
     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
   else:
     print('Usage: eating_cookies.py [num_cookies]')
+
+# print(eating_cookies(100))
